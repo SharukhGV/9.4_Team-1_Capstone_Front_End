712e787d15c62e24635839963e956b3223868aa7
SuperNinjaEv <evan0210@gmail.com>
2023-08-21 19:13:35 -0400

    App created, cleaned up files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import craftopiaLogo from '/Craftopia-Circular-Logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={craftopiaLogo} className="logo react" alt="React logo" />
+      </div>
+      <h1>Craftopia</h1>
+    </>
+  )
+}
+
+export default App
