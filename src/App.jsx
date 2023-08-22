e8c61f08ab7a84d2e931fad80f896783e69c97c5
SuperNinjaEv <evan0210@gmail.com>
2023-08-21 20:50:56 -0400

    added file structure folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import craftopiaLogo from '/Craftopia-Circular-Logo.svg'
+import craftopiaLogo from './assets/Craftopia-Circular-Logo.svg'
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div className='App'>
       <div>
-        <img src={craftopiaLogo} className="logo react" alt="React logo" />
+        <img src={craftopiaLogo} className="logo" alt="logo" />
       </div>
       <h1>Craftopia</h1>
-    </>
+
+      <main>
+        <Routes>
+
+
+
+
+        </Routes>
+      </main>
+    </div>
   )
 }
 
