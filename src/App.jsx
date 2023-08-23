95458787f36c3fbfc7db38644ccaf61b7e708c09
MichelBDLC <michelbdlc@gmail.com>
2023-08-23 14:26:46 -0700

    all header completed & seperated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import craftopiaLogo from './assets/Craftopia-Circular-Logo.svg';
-import ArtistsGraphic from './assets/artistsgraphic.jpg';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Auth from './components/auth/Auth';
 import Landing from './pages/landing/Landing';
-
-//will need to import fonts library
 
 function App() {
 
   return (
     <div className='App'>
-      <Auth craftopiaLogo={craftopiaLogo} />
-      <header>
-        <div className='header-branding'>
-        <img className='home-logo' src={craftopiaLogo} alt='craftopia logo' />
-        <h2> Ignite Your Creativity, Equip Your Creativity </h2>
-        <h4> Explore Gear & Community to Cultivate Inspired Artistry </h4>
-        </div>
-        <img src={ArtistsGraphic} className='artists-graphic' />
-      </header>
       <main>
         <Routes>
-          {/* <Route path='/' element={<Landing />} /> */}
+          <Route path='/' element={<Landing />} />
         </Routes>
       </main>
     </div>
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,10 +1,36 @@
+import './landing.css';
+import Auth from '../../components/auth/Auth';
+import ArtistsGraphic from '../../assets/artistsgraphic.jpg';
+import craftopiaLogo from '../../assets/Craftopia-Circular-Logo.svg';
 
 export default function Landing() {
 
     return (
-
-        <div>
-            Landing
+        <>
+        <Auth craftopiaLogo={craftopiaLogo} />
+        <header>
+        <div className='header-branding'>
+        <img className='home-logo' src={craftopiaLogo} alt='craftopia logo' />
+        <h2 className='header-h2'> Ignite Your Creativity, Equip Your Creativity </h2>
+        <h4 className='header-h4'> Explore Gear & Community to Cultivate Inspired Artistry </h4>
+        <br />
+        <img src={ArtistsGraphic} className='artists-graphic' />
         </div>
+        <br />
+        <div className='header-points'>
+          <div className='point-column'>
+            <p> Discover a vast collection of high-quality gear & equipment tailored to your creative interests</p>
+            <p> Cultivate your skills & creativity by leveraging the diverse range of tools & resources available</p>
+          </div>
+          <div className='point-column'>
+            <p> Connect with fellow hobbyists & enthusiasts who share your passion & expertise</p>
+            <p> Enhance your artistic journey by exchanging ideas, tips, & experience with a supportive community</p>
+          </div>
+        </div>
+        </header>
+        <main>
+            
+        </main>
+        </> 
     )
 }
