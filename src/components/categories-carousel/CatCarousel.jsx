b05e89868f31cc0b38d63b1bb78d5088763c0add
MichelBDLC <michelbdlc@gmail.com>
2023-08-25 10:50:58 -0700

    seperated category carousel component in order to apply to different pages

---
+++ src/components/categories-carousel/CatCarousel.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import './catCarousel.css';
+
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+import ceramicsImg from '../../assets/categoryImg/ceramicsImg.jpg';
+import graffitiImg from '../../assets/categoryImg/graffitiImg.jpg';
+import paintingImg from '../../assets/categoryImg/paintingImg.jpg';
+import photographyImg from '../../assets/categoryImg/photographyImg.jpg';
+import filmmakingImg from '../../assets/categoryImg/filmmakingImg.jpg';
+import fashionDesignImg from '../../assets/categoryImg/fashionDesignImg.jpg';
+import drawingImg from '../../assets/categoryImg/drawingImg.jpg';
+import digitalArtImg from '../../assets/categoryImg/digitalArtImg.jpg';
+import sculptureImg from '../../assets/categoryImg/sculptureImg.jpg';
+import printmakingImg from '../../assets/categoryImg/printmakingImg.jpg';
+
+export default function CatCarousel() {
+
+    const [currentImg, setCurrentImg] = useState(0);
+
+    const artsImgs = [
+        { name: 'Ceramics', imageUrl: ceramicsImg },
+        { name: 'Photography', imageUrl: photographyImg }, 
+        { name: 'Painting', imageUrl: paintingImg },
+        { name: 'Graffiti', imageUrl: graffitiImg },
+        { name: 'Filmmaking', imageUrl: filmmakingImg }, 
+        { name: 'Fashion Design', imageUrl: fashionDesignImg }, //change this image
+        { name: 'Drawing', imageUrl: drawingImg }, 
+        { name: 'Digital Artistry', imageUrl: digitalArtImg },
+        { name: 'Sculpturing', imageUrl: sculptureImg },
+        { name: 'Printmaking', imageUrl: printmakingImg }, //consider replacement
+    ] 
+
+    function prevSlide() {
+        setCurrentImg((prevImg) => (prevImg === 0 ? artsImgs.length - 1 : prevImg - 1));
+    }
+    
+    function nextSlide() {
+        setCurrentImg((prevImg) => (prevImg === artsImgs.length - 1 ? 0 : prevImg + 1));
+    }
+    
+    const visibleImgs = [
+        artsImgs[(currentImg - 1 + artsImgs.length) % artsImgs.length],
+        artsImgs[currentImg],
+        artsImgs[(currentImg + 1) % artsImgs.length],
+        artsImgs[(currentImg + 2) % artsImgs.length],
+        artsImgs[(currentImg + 3) % artsImgs.length],
+    ];
+
+    return (
+        <div className='slider-container'>
+            <button onClick={prevSlide} className='arrow'> <ArrowBackIosIcon /> </button>
+            {
+              visibleImgs.map(img => (
+                <div className='imgDiv' key={img.name}>
+                  <img src={img.imageUrl} alt={img.name} className='categoryImg' loading='lazy' />
+                  <p className='img-name'> {img.name} </p>
+                </div> 
+              ))
+            }
+            <button onClick={nextSlide} className='arrow'> <ArrowForwardIosIcon /> </button>
+        </div>
+        
+    )
+}
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -6,53 +6,14 @@ import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import CardOverflow from '@mui/joy/CardOverflow';
 
+import CatCarousel from '../../components/categories-carousel/CatCarousel';
+
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArtistsGraphic from '../../assets/artistsgraphic.jpg';
 import craftopiaLogo from '../../assets/Craftopia-Circular-Logo.svg';
-import ceramicsImg from '../../assets/categoryImg/ceramicsImg.jpg';
-import graffitiImg from '../../assets/categoryImg/graffitiImg.jpg';
-import paintingImg from '../../assets/categoryImg/paintingImg.jpg';
-import photographyImg from '../../assets/categoryImg/photographyImg.jpg';
-import filmmakingImg from '../../assets/categoryImg/filmmakingImg.jpg';
-import fashionDesignImg from '../../assets/categoryImg/fashionDesignImg.jpg';
-import drawingImg from '../../assets/categoryImg/drawingImg.jpg';
-import digitalArtImg from '../../assets/categoryImg/digitalArtImg.jpg';
-import sculptureImg from '../../assets/categoryImg/sculptureImg.jpg';
-import printmakingImg from '../../assets/categoryImg/printmakingImg.jpg';
 
 export default function Landing() {
-
-  const artsImgs = [
-    { name: 'Ceramics', imageUrl: ceramicsImg },
-    { name: 'Photography', imageUrl: photographyImg }, 
-    { name: 'Painting', imageUrl: paintingImg },
-    { name: 'Graffiti', imageUrl: graffitiImg },
-    { name: 'Filmmaking', imageUrl: filmmakingImg }, 
-    { name: 'Fashion Design', imageUrl: fashionDesignImg }, //change this image
-    { name: 'Drawing', imageUrl: drawingImg }, 
-    { name: 'Digital Artistry', imageUrl: digitalArtImg },
-    { name: 'Sculpturing', imageUrl: sculptureImg },
-    { name: 'Printmaking', imageUrl: printmakingImg }, //consider replacement
-  ] 
-
-  const [currentImg, setCurrentImg] = useState(0);
-
-  function prevSlide() {
-    setCurrentImg((prevImg) => (prevImg === 0 ? artsImgs.length - 1 : prevImg - 1));
-  }
-
-  function nextSlide() {
-    setCurrentImg((prevImg) => (prevImg === artsImgs.length - 1 ? 0 : prevImg + 1));
-  }
-
-  const visibleImgs = [
-    artsImgs[(currentImg - 1 + artsImgs.length) % artsImgs.length],
-    artsImgs[currentImg],
-    artsImgs[(currentImg + 1) % artsImgs.length],
-    artsImgs[(currentImg + 2) % artsImgs.length],
-    artsImgs[(currentImg + 3) % artsImgs.length],
-  ];
 
     return (
         <>
@@ -79,38 +40,27 @@ export default function Landing() {
         </header>
         <br />
         <main>
-          <div className='slider-container'>
-              <button onClick={prevSlide} className='arrow'> <ArrowBackIosIcon /> </button>
-            {
-              visibleImgs.map(img => (
-                <div className='imgDiv' key={img.name}>
-                  <img src={img.imageUrl} alt={img.name} className='categoryImg' loading='lazy' />
-                  <p className='img-name'> {img.name} </p>
-                </div> 
-              ))
-            }
-              <button onClick={nextSlide} className='arrow'> <ArrowForwardIosIcon /> </button>
-          </div>
+          <CatCarousel />
           <br />
           <div>
           <h3 className='top-categories-h3'> Top Categories </h3>
           <br />
           <div >
           <p className='top-category-p'> Photography </p>
-          <button onClick={prevSlide} className='arrow'> <ArrowBackIosIcon /> </button>
-          <button onClick={nextSlide} className='arrow'> <ArrowForwardIosIcon /> </button>
+          <button className='arrow'> <ArrowBackIosIcon /> </button>
+          <button className='arrow'> <ArrowForwardIosIcon /> </button>
           </div>
           {/* <div className='div' /> */}
           <div >
           <p className='top-category-p'> Painting </p>
-          <button onClick={prevSlide} className='arrow'> <ArrowBackIosIcon /> </button>
-          <button onClick={nextSlide} className='arrow'> <ArrowForwardIosIcon /> </button>
+          <button className='arrow'> <ArrowBackIosIcon /> </button>
+          <button className='arrow'> <ArrowForwardIosIcon /> </button>
           </div>
           {/* <div className='div' /> */}
           <div>
           <p className='top-category-p'> Digital Art </p>
-          <button onClick={prevSlide} className='arrow'> <ArrowBackIosIcon /> </button>
-          <button onClick={nextSlide} className='arrow'> <ArrowForwardIosIcon /> </button>
+          <button className='arrow'> <ArrowBackIosIcon /> </button>
+          <button className='arrow'> <ArrowForwardIosIcon /> </button>
           </div>
           {/* <div className='div' /> */}
           </div>
