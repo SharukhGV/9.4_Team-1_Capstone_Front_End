acc9840daba399cfcd6f7fca345afa61cb8db4ea
SuperNinjaEv <evan0210@gmail.com>
2023-08-30 17:20:42 -0400

    created html tags for elements

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,24 @@
+import './navbar.css'
 import Logo from '../../assets/Craftopia-Circular-Logo.svg';
 
 export default function NavBar() {
 
+    handleLoginClick = () => {
+        console.log('Login button clicked');
+      }
+    
+      handleSignUpClick = () => {
+        console.log('Sign Up button clicked');
+      }
+
     return (
-        <>
-        <img src={Logo} />
-        </>
+        <div className='navbar'>
+        <img src={Logo} alt='circ-logo' className='navbar-logo' />
+
+        <input type="text" placeholder="Search..." />
+
+        <button onClick={this.handleLoginClick}>Login</button>
+        <button onClick={this.handleSignUpClick}>Sign Up</button>
+        </div>
     )
 }
