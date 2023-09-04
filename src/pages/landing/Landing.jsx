bac26d7f5e7438a3daa98a0e1977bdf0f6514cc0
MichelBDLC <michelbdlc@gmail.com>
2023-09-04 16:43:13 -0700

    made signup form a component for repeated use

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,9 +1,7 @@
 import './landing.css';
-import Auth from '../../components/auth/Auth';
+import SignUp from '../../components/auth/signUp';
 import { useState } from 'react';
 import NavBar from '../../components/navbar/NavBar';
-import { Box, Modal, TextField } from '@mui/material';
-
 
 //docs for cards: https://mui.com/joy-ui/react-card/ 
 import Card from '@mui/joy/Card';
@@ -21,65 +19,31 @@ import craftopiaLogo from '../../assets/Craftopia-Circular-Logo.svg';
 
 export default function Landing() {
 
-  const [joinOpen, setJoinOpen] = useState(false);
+   const [joinOpen, setJoinOpen] = useState(false);
 
-  const styleJoin = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 390,
-    height: 690,
-    bgcolor: '#f8f8f8',
-    border: '1px solid #D1C4E9',
-    boxShadow: 14,
-    p: 4,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  };
+  return (
+    <>
 
-    return (
-        <>
         {/* <NavBar /> */}
-        <header>
-        <div className='header-branding'>
+    <header>
+      <div className='header-branding'>
         <Card sx={{ width: '87%' }} >
-        {/* border: 1, borderColor: '#D1C4E9',
-        <img className='landing-logo' src={craftopiaLogo} alt='craftopia logo' /> */}
         <div className='landing-action'>  
           <div className='branding-text'>
           <h2 className='header-h2'> Ignite Your Creativity </h2>
           <h4 className='header-h4'> Explore Gear & Community to Cultivate Inspired Artistry </h4>
           <button className='join-btn' onClick={() => setJoinOpen(true)} > Join The Party </button>
-          <Modal open={joinOpen} onClose={() => setJoinOpen(false)}>
-            <Box sx={styleJoin}>
-            <button className='cancel-login' onClick={() => setJoinOpen(false)}> &times; </button>
-            <img src={craftopiaLogo} className='logo-login'/>
-            <br />
-            <form className='login-form'>
-              <TextField variant='standard' label='Name' style={{ width: '300px' }} />
-              <TextField variant='standard' label='Email' style={{ width: '300px' }} />
-              <TextField variant='standard' label='DOB' style={{ width: '300px' }} />
-              <TextField variant='standard' label='Username' style={{ width: '300px' }} />
-              <TextField variant='standard' label='City, State' style={{ width: '300px' }} />
-              <TextField variant='standard' label='Password' style={{ width: '300px' }} />
-              <TextField variant='standard' label='Confirm Password' style={{ width: '300px' }} />
-              <button type='submit' className='login-btn' > Sign Up </button>
-            </form> 
-            </Box>
-          </Modal>
+          <SignUp joinOpen={joinOpen} setJoinOpen={setJoinOpen} craftopiaLogo={craftopiaLogo} />
           </div>
           <div>
           <img src={ArtistsGraphic} className='artists-graphic' />
           </div>
         </div>
         </Card >
-        </div>
+      </div>
         <br />
-        {/* <div className='div' /> */}
         <br />
-        </header>
+    </header>
         <br />
         <main>
           <CatCarousel />
@@ -92,22 +56,19 @@ export default function Landing() {
           <button className='arrow'> <ArrowBackIosIcon /> </button>
           <button className='arrow'> <ArrowForwardIosIcon /> </button>
           </div>
-          {/* <div className='div' /> */}
           <div >
           <p className='top-category-p'> Painting </p>
           <button className='arrow'> <ArrowBackIosIcon /> </button>
           <button className='arrow'> <ArrowForwardIosIcon /> </button>
           </div>
-          {/* <div className='div' /> */}
           <div>
           <p className='top-category-p'> Digital Art </p>
           <button className='arrow'> <ArrowBackIosIcon /> </button>
           <button className='arrow'> <ArrowForwardIosIcon /> </button>
           </div>
-          {/* <div className='div' /> */}
           </div>
           <br />
         </main>
-        </> 
-    )
+    </> 
+  )
 }
