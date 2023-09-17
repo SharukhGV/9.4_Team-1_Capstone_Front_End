4661793573651ce09082a753ea92e863c659e96f
Felizj17 <felizj17@gmail.com>
2023-09-17 15:43:20 -0400

    css form added and file preview added for tools thumbnail

--- src/components/tools/ToolsNewForm.jsx
+++ src/components/tools/ToolsNewForm.jsx
@@ -2,12 +2,16 @@ import {useState, useEffect} from 'react';
 import {useNavigate, Link} from 'react-router-dom';
 import axios from 'axios';
 import { Select, TextField, MenuItem } from '@mui/material';
+import thumbnailPlaceHolder from '../../assets/thumbnail.jpeg'
 import './toolsForm.css'
 function ToolsNewForm({user}) {
   const API = import.meta.env.VITE_REACT_APP_API_URL;
-  const [files, setFiles] = useState({});
+  const [thumbnail, setThumbnail] = useState({
+    preview:'',
+    data:''
+  });
 
-  const [tool, settool] = useState({
+  const [tool, setTool] = useState({
     user_id: user.user_id,
     name_tools: '',
     item_condition: '',
@@ -18,65 +22,56 @@ function ToolsNewForm({user}) {
   });
   const navigate = useNavigate();
 
-  const addtool = newtool => {
-    console.log(files['thumbnail']);
+  const addTool = newTool => {
     const newForm = new FormData();
     newForm.append('thumbnail', files['thumbnail']);
-
-    // newForm.append("user_id", user.user_id)
-    for (const key in newtool) {
-      newForm.append(key, newtool[key]);
+    for (const key in newTool) {
+      newForm.append(key, newTool[key]);
     }
-    console.log(newForm);
     axios
       .post(`${API}/tools`, newForm, {
         headers: {'Content-Type': 'multipart/form-data'},
       })
       .then(response => {
+
         console.log(response.data);
-        // console.log(newForm)
-        // settool(response.data); // set the entire `tool` object
-        // navigate(`/${user}/tools`);
       })
       .catch(e => console.error('catch', e));
   };
 
-  function handleFileUploads(event) {
-    setFiles({...files, [event.target.name]: event.target.files[0]});
+  function handleThumbnail(event) {
+    setThumbnail({
+      preview:URL.createObjectURL(event.target.files[0]),
+      data: event.target.files[0]
+    });
   }
 
   const handleTextChange = event => {
     if (event.target.id === 'price' || event.target.id === 'stock_quantity') {
-      settool({...tool, [event.target.id]: Number(event.target.value)});
+      setTool({...tool, [event.target.id]: Number(event.target.value)});
     }
-    //   if (event.target.id === "stock_quantity") {
-    //     settool({ ...tool, [event.target.id]: Number(event.target.value) });
-    //   }
     else {
-      settool({...tool, [event.target.id]: event.target.value});
+      setTool({...tool, [event.target.id]: event.target.value});
     }
-
-    // settool({ ...tool, [event.target.id]: event.target.value });
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    addtool(tool);
+    addTool(tool);
   };
 
   return (
     <div className='edit'>
       <form className='tool-form' onSubmit={handleSubmit}>
-        {/* <TextField type="hidden" id="user_id" name="user_id" value={userShow2}></TextField> */}
-        <label htmlFor='thumbnail'>Picture of Tool:</label>
-
+        <label htmlFor='thumbnail'>Thumbnail</label>
+        <img src={thumbnail.preview?thumbnail.preview:thumbnailPlaceHolder} alt='thumbnail-preview' style={{width:'200px', height:'auto'}} />
         <input
           type='file'
           id='thumbnail-input'
           name='thumbnail'
           accept='image/*'
           alt='thumbnail input'
-          onChange={handleFileUploads}
+          onChange={handleThumbnail}
         />
 
         <TextField
