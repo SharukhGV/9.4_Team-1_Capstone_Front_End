8d34ec3f71719b54a6374258db15e8127bb736e4
SharukhGV <sharukhvelupillai@pursuit.org>
2023-09-19 01:45:03 -0400

    radio buttons assesssment

--- src/components/assesment/Assesment.jsx
+++ src/components/assesment/Assesment.jsx
@@ -1,29 +1,66 @@
-import {Modal, Box, FormGroup, FormControlLabel, Checkbox} from '@mui/material';
-import './assesment.css';
-
+import {
+  Modal,
+  Box,
+  RadioGroup,
+  Radio,
+  FormLabel,
+  FormControlLabel,
+  FormControl,
+  Checkbox,
+} from "@mui/material";
+import "./assesment.css";
+import { useState } from "react";
+import axios from "axios";
 export default function Assesment(props) {
   const assesmentModalStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '77%',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "77%",
     height: 490,
-    bgcolor: '#f8f8f8',
-    border: '1px solid #D1C4E9', //change this
+    bgcolor: "#f8f8f8",
+    border: "1px solid #D1C4E9", //change this
     boxShadow: 14,
     p: 4,
   };
+  const API = import.meta.env.VITE_REACT_APP_API_URL;
+
+  const [updatedUser, setUpdatedUser] = useState(props.user); // Initialize with the default selected value
+
+  const handleRadioChange = (event) => {
+    // console.log(event.target.name)
+    // console.log(updatedUser)
+
+    setUpdatedUser({
+      ...updatedUser,
+      [event.target.name]: event.target.value
+    }); // Update the selected value when the radio button changes
+  };
+
 
   function handleAssesmentBtns(event) {
     event.preventDefault();
 
     props.setAssesmentModalOpen(false);
 
-    if (event.target.value === 'later-btn') {
+    if (event.target.value === "later-btn") {
       props.setAssesmentCompleted(false);
     } else {
-      props.setAssesmentCompleted(true);
+
+
+
+      axios
+      .put(`${API}/auth/${props.user.user_id}`,updatedUser)
+      .then((response) => {
+        console.log(response.data.updatedAccount);
+
+      })
+      .catch((e) => console.error(e));
+      // console.log(updatedUser.learning_interest);
+      // console.log(updatedUser.current_skillset);
+
+      // props.setAssesmentCompleted(true);
     }
   }
 
@@ -36,129 +73,156 @@ export default function Assesment(props) {
         <Box sx={assesmentModalStyle}>
           <button
             onClick={() => props.setAssesmentModalOpen(false)}
-            className='x-btn'
+            className="x-btn"
           >
-            {' '}
-            &times;{' '}
+            {" "}
+            &times;{" "}
           </button>
           <p>
-            {' '}
+            {" "}
             These quick assesment questions will help us curate your homapage
-            with suggestions on equipment, hobby discoveries & more{' '}
+            with suggestions on equipment, hobby discoveries & more{" "}
           </p>
-          <div className='questions-sect'>
+          <div className="questions-sect">
             <div>
               <p>
-                {' '}
+                {" "}
                 Which virtual arts hobbies are you interested in learning /
-                exploring?{' '}
+                exploring?{" "}
               </p>
-              <FormGroup>
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Painting'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Drawing / Sketching'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Photography / Filmmaking'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Pottery / Ceramics'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Digital Art'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Sculpture / 3D Art'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Printmaking'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Fashion Design'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Graffiti'
-                />
-              </FormGroup>
+              <FormControl>
+                <FormLabel id="demo-radio-buttons-group-label">
+                  Gender
+                </FormLabel>
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  // value={updatedUser.learning_interest}
+                  name="learning_interest"
+                  onChange={(e)=>handleRadioChange(e)}
+                  id="learning_interest"
+                >
+                  <FormControlLabel
+                    value="painting"
+                    control={<Radio />}
+                    label="painting"
+                  />
+                  <FormControlLabel
+                    value="drawing"
+                    control={<Radio />}
+                    label="drawing"
+                  />
+                  <FormControlLabel
+                    value="photography"
+                    control={<Radio />}
+                    label="photography"
+                  />
+                  <FormControlLabel
+                    value="pottery"
+                    control={<Radio />}
+                    label="pottery"
+                  />
+                  <FormControlLabel
+                    value="sculpture"
+                    control={<Radio />}
+                    label="sculpture"
+                  />
+                  <FormControlLabel
+                    value="printmaking"
+                    control={<Radio />}
+                    label="printmaking"
+                  />
+                  <FormControlLabel
+                    value="fashion"
+                    control={<Radio />}
+                    label="fashion"
+                  />
+                  <FormControlLabel
+                    value="graffiti"
+                    control={<Radio />}
+                    label="graffiti"
+                  />
+                </RadioGroup>
+              </FormControl>
             </div>
             {/* <br /> */}
             <div>
               <p>
-                {' '}
-                Which virtual arts hobbies do you have good experience in?{' '}
+                {" "}
+                Which virtual arts hobbies do you have good experience in?{" "}
               </p>
-              <FormGroup>
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Painting'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Drawing / Sketching'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Photography / Filmmaking'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Pottery / Ceramics'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Digital Art'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Sculpture / 3D Art'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Printmaking'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Fashion Design'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Graffiti'
-                />
-                <FormControlLabel
-                  control={<Checkbox size='xsmall' />}
-                  label='Beginner'
-                />
-              </FormGroup>
+              <FormControl>
+                <FormLabel id="demo-radio-buttons-group-label">
+                  Gender
+                </FormLabel>
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  // value={updatedUser.current_skillset}
+                  name="current_skillset"
+                  onChange={(e)=>handleRadioChange(e)}
+                  id="current_skillset"
+
+                >
+                  <FormControlLabel
+                    value="painting"
+                    control={<Radio />}
+                    label="painting"
+                  />
+                  <FormControlLabel
+                    value="drawing"
+                    control={<Radio />}
+                    label="drawing"
+                  />
+                  <FormControlLabel
+                    value="photography"
+                    control={<Radio />}
+                    label="photography"
+                  />
+                  <FormControlLabel
+                    value="pottery"
+                    control={<Radio />}
+                    label="pottery"
+                  />
+                  <FormControlLabel
+                    value="sculpture"
+                    control={<Radio />}
+                    label="sculpture"
+                  />
+                  <FormControlLabel
+                    value="printmaking"
+                    control={<Radio />}
+                    label="printmaking"
+                  />
+                  <FormControlLabel
+                    value="fashion"
+                    control={<Radio />}
+                    label="fashion"
+                  />
+                  <FormControlLabel
+                    value="graffiti"
+                    control={<Radio />}
+                    label="graffiti"
+                  />
+                </RadioGroup>
+              </FormControl>
             </div>
           </div>
           <br />
-          <div className='btns'>
+          <div className="btns">
             <button
-              className='later-btn'
-              value='later-btn'
-              onClick={event => handleAssesmentBtns(event)}
+              className="later-btn"
+              value="later-btn"
+              onClick={(event) => handleAssesmentBtns(event)}
             >
-              {' '}
-              Complete Later{' '}
+              {" "}
+              Complete Later{" "}
             </button>
             <button
-              className='done-btn'
-              value='done-btn'
-              onClick={event => handleAssesmentBtns(event)}
+              className="done-btn"
+              value="done-btn"
+              onClick={(event) => handleAssesmentBtns(event)}
             >
-              {' '}
-              Done{' '}
+              {" "}
+              Done{" "}
             </button>
           </div>
         </Box>
