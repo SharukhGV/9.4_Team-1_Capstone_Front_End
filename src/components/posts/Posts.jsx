e5a92856fd4596395f70f7f3b28bf92944b0674e
MichelBDLC <michelbdlc@gmail.com>
2023-09-21 02:11:32 -0700

    posts

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -12,80 +12,46 @@ function Posts() {
   const [posts, setposts] = useState([]);
   const [currentPost, setCurrentPost] = useState(0);
   const [visiblePosts, setVisiblePosts] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getPosts = () => {
     axios.get(`${API}/posts`)
     .then((response) => {
-        setposts(response.data);
+      //console.log(response.data);
+      const allPosts = response.data;
+      const theVisiblePosts = [
+        allPosts[(currentPost - 1 + allPosts.length) % allPosts.length],
+        allPosts[currentPost],
+        allPosts[(currentPost + 1) % allPosts.length],
+        allPosts[(currentPost + 2) % allPosts.length],
+        allPosts[(currentPost + 3) % allPosts.length],
+      ];
+      //console.log(theVisiblePosts)
+      setVisiblePosts(theVisiblePosts);
+      setposts(response.data);
     })
     .catch(error => console.error('catch', error))
-    .finally(() => {
-      const theVisiblePosts = [
-        posts[(currentPost - 1 + posts.length) % posts.length],
-        posts[currentPost],
-        posts[(currentPost + 1) % posts.length],
-        posts[(currentPost + 2) % posts.length],
-        posts[(currentPost + 3) % posts.length],
-      ];
-      setVisiblePosts(theVisiblePosts);
-      // updateVisiblePosts();
-      // setLoading(false);
-    })
   }
   getPosts();
 }, []);
 
-// useEffect(() => {
-//   console.log(`loggin posts ${posts}`);
-// }, [posts])
+  function prevSlide() {
+    setCurrentPost(prevPost =>
+      prevPost === 0 ? posts.length - 1 : prevPost - 1
+    );
+  } 
 
-// useEffect(() => {
-//   console.log(`logging visibleposts ${visiblePosts}`)
-// }, [visiblePosts])
-
-  // function updateVisiblePosts() {
-  //     theVisiblePosts = [
-  //     posts[(currentPost - 1 + posts.length) % posts.length],
-  //     posts[currentPost],
-  //     posts[(currentPost + 1) % posts.length],
-  //     posts[(currentPost + 2) % posts.length],
-  //     posts[(currentPost + 3) % posts.length],
-  //   ];
-  //   //setVisiblePosts(theVisiblePosts);
-  // }
-
-  // function prevSlide() {
-  //   setCurrentPost(prevPost =>
-  //     prevPost === 0 ? posts.length - 1 : prevPost - 1
-  //   );
-  //   updateVisiblePosts();
-  // } 
-
-  // function prevSlide() {
-  //   setCurrentPost(prevPost =>
-  //     prevPost === 0 ? posts.length - 1 : prevPost - 1
-  //   );
-  //   updateVisiblePosts();
-  // } 
-
-  // function nextSlide() {
-  //   setCurrentPost(prevPost =>
-  //     prevPost === posts.length - 1 ? 0 : prevPost + 1
-  //   );
-  //   updateVisiblePosts();
-  // } 
-
-  //console.log(posts)
-  //theres also post.category
-  //or we can just make the whole thing show dique 'most popular'
+  function nextSlide() {
+    setCurrentPost(prevPost =>
+      prevPost === posts.length - 1 ? 0 : prevPost + 1
+    );
+  } 
 
   return (
     <>
     <h4> Top User Posts </h4>
     <div className='slider-container'>
-      <button className='arrow' >{' '} <ArrowBackIosIcon />{' '} </button>
+      <button className='arrow' onClick={prevSlide}>{' '} <ArrowBackIosIcon />{' '} </button>
       { 
         visiblePosts.map((post, i) => (
           <Card component='li' variant='solid' key={`post-${i}`} sx={{ height: 119 }}  >
@@ -94,7 +60,7 @@ function Posts() {
               <img loading='lazy' />
               </AspectRatio>
             <CardContent>
-              {/* <Typography> {post.title} </Typography> */}
+              <Typography> {post?.title} </Typography>
               <Typography> This is post description </Typography>
               {/* <p> {post?.title || 'Loading...'} </p> */}
               {/* <p> post description? try typography comp </p> */}
@@ -103,28 +69,12 @@ function Posts() {
           </Card>
         ))
       }
-       <button className='arrow' >
+       <button className='arrow' onClick={nextSlide} >
         {' '}
         <ArrowForwardIosIcon />{' '}
       </button>
     </div>
     </>
-    // <Carousel>
-    //   {posts.map((individualpost, index) => {
-    //     return (
-    //       <PostsCard
-    //         inKEY={individualpost.user_id}
-    //         title={individualpost.title}
-    //         tags={individualpost.tags}
-    //         body={individualpost.body}
-    //         edited={individualpost.edited_at}
-    //         createdTime={individualpost.created_at}
-    //         thumbnail={individualpost.thumbnail}
-    //         index={index}
-    //       />
-    //     );
-    //   })}
-    // </Carousel>
   );
 }
 
