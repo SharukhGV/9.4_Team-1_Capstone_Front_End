f9f84e5f9a9a2e1de010aee0d45f4671f7ca24f7
SuperNinjaEv <evan0210@gmail.com>
2023-09-30 17:39:00 -0400

    Skeleton of about section complete

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,33 @@
 export default function About() {
   return (
-    <>
-      <p> About </p>
-    </>
+    <div className="about">
+    <div className="about-logo">
+      <img src="path_to_your_logo_image" alt="Craftopia Logo" />
+    </div>
+    <h2>About our App</h2>
+    <div className="app-description">
+      {/* Description of your app */}
+    </div>
+    <img src="path_to_another_image" alt="Another Image" />
+    <h2>Meet the Team</h2>
+    <img src="path_to_team_member_image_1" alt="Team Member 1" />
+    <div className="team-member-description">
+      {/* Description of team member #1 */}
+    </div>
+    <img src="path_to_team_member_image_2" alt="Team Member 2" />
+    <div className="team-member-description">
+      {/* Description of team member #2 */}
+    </div>
+    <img src="path_to_team_member_image_3" alt="Team Member 3" />
+    <div className="team-member-description">
+      {/* Description of team member #3 */}
+    </div>
+    <img src="path_to_team_member_image_4" alt="Team Member 4" />
+    <div className="team-member-description">
+      {/* Description of team member #4 */}
+    </div>
+    <img src="path_to_another_image" alt="Another Image" />
+    <img src="path_to_another_image" alt="Another Image" />
+  </div>
   );
 }
